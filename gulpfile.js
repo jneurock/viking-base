baffa3878252ee8f5a3f584fd0cf3313ce623207
Rocky Neurock <jneurock@gmail.com>
2014-05-16 22:39:47 -0700

    Cleaned up gulpfile.js and gulp build a little bit

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,16 @@
 /*
  * Viking Base registers gulp tasks for you.
- * The gulp tasks are defined in a simple hash.
- * More tasks can be added or existing tasks can be modified.
- * The task hash looks like:
- * {
- *   myTask: {
- *     callback: function() {
- *
- *       return gulp.src()
- *         .pipe( gulp.dest() ); 
- *     },
- *     depends: ['']
- *   }
- * }
- *
- * The property name added to the hash will serve as the task name.
- * Dependencies can be added or changed by specifying an array of 
- * strings that match task names.
- * The callback specifies what gulp should do.
- *
- * Modify tasks as you need before calling vb.registerGulpTasks.
- *
- * To see the hash of configurable options, refer to:
- * bower_components/viking-base/node_modules/viking-base/index.js
+ * For more information and examples see the
+ * project on GitHub (https://github.com/jneurock/viking-base).
  */
 
-var es = require('event-stream'),
-    gulp = require('gulp'),
+var gulp = require('gulp'),
     plugins = require('gulp-load-plugins')(),
     vb = require('./bower_components/viking-base/node_modules/viking-base');
 
 // Set module refeneces on vb object
-vb.es = es;
 vb.gulp = gulp;
 vb.plugins = plugins;
 
-// Modify tasks here
-
-// Call vb.registerGulpTasks after you've made any task modifications
+// Modify tasks as you need before calling vb.registerGulpTasks
 vb.registerGulpTasks();
